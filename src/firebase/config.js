1e948c0099696fdfde04794cbcaf2df1158c4598
billy23 <billmwas2@gmail.com>
2020-10-13 17:01:38 +0300

    fixed .gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCmoHElG4dpgP8VsXJ2FDdSTC-hd8QTMzE",
-  authDomain: "firegram-5f437.firebaseapp.com",
-  databaseURL: "https://firegram-5f437.firebaseio.com",
-  projectId: "firegram-5f437",
-  storageBucket: "firegram-5f437.appspot.com",
-  messagingSenderId: "1012235796944",
-  appId: "1:1012235796944:web:4fd1636fb1acce085bb9d5"
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.apiKey,
+  databaseURL: process.env.apiKey,
+  projectId: process.env.apiKey,
+  storageBucket: process.env.apiKey,
+  messagingSenderId: process.env.apiKey,
+  appId: process.env.apiKey
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
